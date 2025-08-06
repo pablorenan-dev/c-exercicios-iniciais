34b3129470b559cc517b84401c43aecaf14a4b6a
pablorenan-dev <pablonichterwistz@outlook.com>
2025-08-06 20:52:56 -0300

    first commit

---
+++ b1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int soma(int *x, int *y, int *result){
+	*result = *x + *y;
+}
+
+int main( void ){
+	int x = 10;
+	int y = 20;
+	int result = 0;
+	
+	soma(&x, &y, &result);
+	
+	printf("SOMA = %d", result);
+}
+
